6ce0c770737a731b584dd3191f64f5884409181f
dmz <daimingzhu12@outlook.com>
2020-12-02 15:36:16 +0800

    api framework

--- wework_api_framework/wework_tag.py
+++ wework_api_framework/wework_tag.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 
@@ -18,6 +20,20 @@ def test_tag():
     print(token)
     print(r.json())
     assert r.json()["errmsg"] == "ok"
+
+    """创建标签"""
+    url2 = "https://qyapi.weixin.qq.com/cgi-bin/tag/create"
+    r_add = requests.post(
+        url=url2,
+        params = {"access_token":token},
+        json={
+            "tagname": "tag_UI_13",
+            "tagid": 13
+    }
+    )
+    # print("创建标签",r_add.json())
+    # # assert r_add.status_code == 200
+
     """
     获取标签列表
     获取标签列表doc url：https://work.weixin.qq.com/api/doc/90000/90135/90216
@@ -25,19 +41,29 @@ def test_tag():
     url1 = 'https://qyapi.weixin.qq.com/cgi-bin/tag/list'
     token_data = {"access_token":token}
     r_list = requests.get(url=url1, params=token_data)
-    print(r_list.json())
+    print("获取标签列表",r_list.json())
     assert r_list.status_code == 200
     assert r_list.json()["errmsg"] == "ok"
 
-    """
-    添加标签列表成员 
-    """
-    url2 = "https://qyapi.weixin.qq.com/cgi-bin/tag/addtagusers"
-    add_data = {
-        "tagid": 12,
-        "userlist":[ "user1","user2"],
-        "partylist": [4]
-        }
-    r_add = requests.post(url=url2, params=add_data, json=token_data)
-    print(r_add.json())
-    assert r_add.status_code == 200
+    """增加标签成员"""
+    url2 = 'https://qyapi.weixin.qq.com/cgi-bin/tag/addtagusers'
+    token_data = {"access_token":token}
+    member_data = {
+        # 需要先创建一个标签，再在这个标签下去添加成员
+        "tagid": 13,
+        # 成员唯一标识：账号，一定要是在通讯录中已经存在的,也就是说，增加标签成员，传的是成员ID
+        "userlist": ["user_tag_01", "user_tag_02"]
+    }
+    r_tag_member = requests.post(url=url2, params=token_data,json=member_data)
+    print("增加标签成员",r_tag_member.json())
+    assert r_tag_member.status_code == 200
+    assert r_tag_member.json()["errmsg"] == "ok"
+
+
+    """获取成员列表"""
+    url1 = 'https://qyapi.weixin.qq.com/cgi-bin/tag/get'
+    param = {"access_token":token, "tagid":13}
+    r_tag_memlist = requests.get(url=url1, params=param)
+    print("获取标签成员",r_tag_memlist.json())
+    assert r_tag_memlist.status_code == 200
+    assert r_tag_memlist.json()["errmsg"] == "ok"
