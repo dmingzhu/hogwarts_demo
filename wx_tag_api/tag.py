86c79e7d55190fd4cd1a2f3683b5b9504df0e214
dmingzhu <393769136@qq.com>
2020-12-07 22:13:51 +0800

    api

--- wx_tag_api/tag.py
+++ wx_tag_api/tag.py
@@ -2,3 +2,64 @@
 # @time    :2020/12/7 20:34
 # @Author  :dmingzhu
 # @dmingzhu:tag.py
+import json
+
+import requests
+
+
+class Tag:
+    """获取token"""
+    def get_token(self):
+        url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
+        param={
+            "corpid":"ww69694e10de384523",
+            "corpsecret":"_yb7L6V2q0Guqk44vdR9ju7fAjDYH8IH6_9nwp7472c"
+        }
+        r = requests.get(url=url, params=param)
+        # print(json.dumps(r.json(), indent = 2))
+        return r.json()["access_token"]
+
+    """获取列表"""
+    def get_corp_tag_list(self, tag_id):
+        url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list"
+        param = {
+            "access_token":self.get_token()
+        }
+        json = {
+            "tag_id": tag_id
+        }
+        r = requests.post(url=url, params=param, json=json)
+        # print(json.dumps(r.json(), indent=2))
+        return r.json()
+
+    """添加标签"""
+    def add_crop_tag(self):
+        url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag"
+        param = {
+             "access_token": self.get_token()
+         }
+        data = {
+        "group_name": "GROUP_NAME_1207",
+        "order": 1,
+        "tag": [{
+            "name": "TAG_NAME_1",
+            "order": 1
+        }
+    ]
+}
+        r = requests.post(url=url , params=param,  json= data)
+        # print(json.dumps(r.json(), indent=2))
+        return r.json()
+
+    def edit_corp_tag(self):
+        pass
+
+    def del_corp_tag(self):
+        pass
+
+
+if __name__ == "__main__":
+    AA = Tag()
+    # AA.get_token()
+    # AA.get_corp_tag_list()
+    AA.add_crop_tag()
--- wx_tag_api/test_tag.py
+++ wx_tag_api/test_tag.py
@@ -2,3 +2,32 @@
 # @time    :2020/12/7 20:34
 # @Author  :dmingzhu
 # @dmingzhu:test_tag.py
+import json
+
+from jsonpath import jsonpath
+
+from wx_tag_api.tag import Tag
+
+
+class TestTag:
+    def setup(self):
+        self.tag = Tag()
+
+    def test_get_corp_tag_list(self):
+        self.tag.get_corp_tag_list()
+
+    def test_add_crop_tag(self):
+        r = self.tag.add_crop_tag()
+        # print(json.dumps(r, indent=2))
+        # 获取tagget_id
+        tag_id = jsonpath(r, "$..tag_group.tag[0].id")[0]
+        r_list = self.tag.get_corp_tag_list(tag_id)
+        # print(json.dumps(r_list, indent=2))
+        assert r_list["tag_group"][0]["tag"][0]["name"] == "TAG_NAME_1"
+
+    def test_edit_corp_tag(self):
+        pass
+
+    def test_del_corp_tag(self):
+        pass
+
