77f51350d6c244767e588f0b4b28f43c2983d444
dmingzhu <393769136@qq.com>
2020-12-07 23:46:17 +0800

    api

--- wx_tag_api/tag.py
+++ wx_tag_api/tag.py
@@ -5,25 +5,36 @@
 import json
 
 import requests
-
+corpid = "ww69694e10de384523"
+corpsecret = "_yb7L6V2q0Guqk44vdR9ju7fAjDYH8IH6_9nwp7472c"
 
 class Tag:
-    """获取token"""
+    """
+    构造一个类变量存token
+    """
+    def __init__(self):
+        self.token = ""
+
+    """
+    封装获取token
+    """
     def get_token(self):
         url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
         param={
-            "corpid":"ww69694e10de384523",
-            "corpsecret":"_yb7L6V2q0Guqk44vdR9ju7fAjDYH8IH6_9nwp7472c"
+            "corpid":corpid,
+            "corpsecret":corpsecret
         }
         r = requests.get(url=url, params=param)
         # print(json.dumps(r.json(), indent = 2))
-        return r.json()["access_token"]
+        self.token =  r.json()["access_token"]
 
-    """获取列表"""
+    """
+    获取列表
+    """
     def get_corp_tag_list(self, tag_id):
         url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/get_corp_tag_list"
         param = {
-            "access_token":self.get_token()
+            "access_token":self.token
         }
         json = {
             "tag_id": tag_id
@@ -32,22 +43,20 @@ class Tag:
         # print(json.dumps(r.json(), indent=2))
         return r.json()
 
-    """添加标签"""
-    def add_crop_tag(self):
+    """
+    添加标签
+    """
+    def add_crop_tag(self, group_name, tag_names):
         url = "https://qyapi.weixin.qq.com/cgi-bin/externalcontact/add_corp_tag"
         param = {
-             "access_token": self.get_token()
+             "access_token": self.token
          }
-        data = {
-        "group_name": "GROUP_NAME_1207",
-        "order": 1,
-        "tag": [{
-            "name": "TAG_NAME_1",
-            "order": 1
-        }
-    ]
-}
-        r = requests.post(url=url , params=param,  json= data)
+        json = {
+            "group_name": group_name,
+            "tag": tag_names
+            }
+
+        r = requests.post(url=url , params=param,  json= json)
         # print(json.dumps(r.json(), indent=2))
         return r.json()
 
@@ -60,6 +69,6 @@ class Tag:
 
 if __name__ == "__main__":
     AA = Tag()
-    # AA.get_token()
+    AA.get_token()
     # AA.get_corp_tag_list()
-    AA.add_crop_tag()
+    # AA.add_crop_tag()
--- wx_tag_api/test_tag.py
+++ wx_tag_api/test_tag.py
@@ -4,6 +4,7 @@
 # @dmingzhu:test_tag.py
 import json
 
+import pytest
 from jsonpath import jsonpath
 
 from wx_tag_api.tag import Tag
@@ -12,18 +13,28 @@ from wx_tag_api.tag import Tag
 class TestTag:
     def setup(self):
         self.tag = Tag()
+        # 调用get_token()给self.token赋值
+        self.tag.get_token()
 
     def test_get_corp_tag_list(self):
         self.tag.get_corp_tag_list()
 
-    def test_add_crop_tag(self):
-        r = self.tag.add_crop_tag()
-        # print(json.dumps(r, indent=2))
-        # 获取tagget_id
-        tag_id = jsonpath(r, "$..tag_group.tag[0].id")[0]
+    @pytest.mark.parametrize("group_name, tag_names", [
+        ("111", [{"name":"222"}])
+    ])
+    def test_add_crop_tag(self, group_name, tag_names):
+        r = self.tag.add_crop_tag(group_name=group_name, tag_names=tag_names)
+        print(json.dumps(r, indent=2))
+        # 获取taget_id，存在列表里
+        tag_id = jsonpath(r, "$..tag_group.tag[0].id")
         r_list = self.tag.get_corp_tag_list(tag_id)
-        # print(json.dumps(r_list, indent=2))
-        assert r_list["tag_group"][0]["tag"][0]["name"] == "TAG_NAME_1"
+        print(json.dumps(r_list, indent=2))
+        # 判断get_list返回值中存在添加的tag name
+        group = [group for group in r_list["tag_group"] if group["group_name"] == group_name][0]
+        tags = [{"name":tag["name"]} for tag in group["tag"]]
+        print(group)
+        print(tags)
+    #   使用的哪几个参数的测试数据，在断言时，需要用对应的实际结果去判断是否输出正确，所以此处需要针对group_name 和tag_name都做断言
 
     def test_edit_corp_tag(self):
         pass
